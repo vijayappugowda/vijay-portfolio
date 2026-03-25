3ff6ca45b2d9a55c547322901776845bb58b9a86
Vijay Kumar B S <vijayappugowda@gmail.com>
2026-03-25 19:53:46 +0530

    Configure GitHub Pages deployment via GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/vijay-portfolio',
+  assetPrefix: '/vijay-portfolio',
 };
 
 export default nextConfig;
